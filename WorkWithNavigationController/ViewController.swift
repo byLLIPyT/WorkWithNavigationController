0680336629ccff9597f1c45972bea6da73a05b34
byLLIPyT <byllipyt@icloud.com>
2021-06-11 06:48:12 +0300

    +finish

--- WorkWithNavigationController/ViewController.swift
+++ WorkWithNavigationController/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //ссылка на сториборд
+    let storyboardInstance = UIStoryboard(name: "Main", bundle: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func toGreenScene(_ sender: Any) {
+        let greenVC = storyboardInstance.instantiateViewController(identifier: "GreenVC")
+        self.navigationController?.pushViewController(greenVC, animated: true)
+    }
+    
+    @IBAction func toYellowScene(_ sender: Any) {
+        let yellowVC = storyboardInstance.instantiateViewController(identifier: "YellowVC")
+        self.navigationController?.pushViewController(yellowVC, animated: true)
+    }
+    
+    @IBAction func toRootScene(_ sender: Any) {
+        let rootVC = storyboardInstance.instantiateViewController(identifier: "RedVC")
+        self.navigationController?.pushViewController(rootVC, animated: true)
+    }
+    
+    @IBAction func toPreviousFromRed(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    
+    @IBAction func toPreviousFromGreen(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    
+    @IBAction func toPreviousFromYellow(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
 }
 
